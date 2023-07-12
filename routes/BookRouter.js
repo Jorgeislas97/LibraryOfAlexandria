9cc922a8aefbce5e748bd608de6de75d395e083f
Jorge Islas <Jorgeislas97@gmail.com>
2023-07-12 16:42:49 -0400

    middleware implemented awaiting test

--- routes/BookRouter.js
+++ routes/BookRouter.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const BookController = require('../controllers/BookController'); 
+const BookController = require('../controllers/BookController');
+const { stripToken, verifyToken } = require('../middleware'); // import middleware
+
 // Route to get all books
-router.get('/Books', BookController.getAll);
+router.get('/Books', stripToken, verifyToken, BookController.getAll);
 
 // Route to get a book by id
-router.get('/Books/:id', BookController.getById);
+router.get('/Books/:id', stripToken, verifyToken, BookController.getById);
 
 // Route to create a new book
-router.post('/Books', BookController.create);
+router.post('/Books', stripToken, verifyToken, BookController.create);
 
 // Route to update a book
-router.put('/Books/:id', BookController.update);
+router.put('/Books/:id', stripToken, verifyToken, BookController.update);
 
 // Route to delete a book
-router.delete('/Books/:id', BookController.delete);
+router.delete('/Books/:id', stripToken, verifyToken, BookController.delete);
 
 module.exports = router;
--- routes/ReviewRoute.js
+++ routes/ReviewRoute.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const ReviewController = require('../controllers/ReviewController');
+const { stripToken, verifyToken } = require('../middleware'); // import middleware
 
 const router = express.Router();
 
 // Routes for reviews
-router.post('/', ReviewController.create);
-router.get('/', ReviewController.getAll);
-router.get('/:id', ReviewController.getById);
-router.put('/:id', ReviewController.update);
-router.delete('/:id', ReviewController.delete);
+router.post('/', stripToken, verifyToken, ReviewController.create); // add middleware to check for token before creating a review
+router.get('/', stripToken, verifyToken, ReviewController.getAll); // only logged in users can view all reviews
+router.get('/:id', stripToken, verifyToken, ReviewController.getById); // only logged in users can view a review by ID
+router.put('/:id', stripToken, verifyToken, ReviewController.update); // only logged in users can update a review
+router.delete('/:id', stripToken, verifyToken, ReviewController.delete); // only logged in users can delete a review
 
 module.exports = router;
