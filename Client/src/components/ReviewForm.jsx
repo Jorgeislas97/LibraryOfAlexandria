1f968bf68fa2ba29d771dc2c36721dbe06e5701e
Jorge Islas <Jorgeislas97@gmail.com>
2023-07-18 14:12:36 -0400

    got form to render

--- Client/src/components/ReviewForm.jsx
+++ Client/src/components/ReviewForm.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const ReviewForm = () => {
+const ReviewForm = ({ bookId, onSubmit }) => {
+  const [rating, setRating] = useState(1);
+  const [reviewComment, setReviewComment] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const review = {
+      book: bookId,
+      rating,
+      reviewComment,
+    };
+    onSubmit(review);
+    setRating(1);
+    setReviewComment('');
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <form onSubmit={handleSubmit}>
+      <label>
+        Rating:
+        <select value={rating} onChange={(e) => setRating(e.target.value)}>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+        </select>
+      </label>
 
-export default ReviewForm
+      <label>
+        Comment:
+        <textarea
+          value={reviewComment}
+          onChange={(e) => setReviewComment(e.target.value)}
+        ></textarea>
+      </label>
+
+      <button type="submit">Submit Review</button>
+    </form>
+  );
+};
+
+export default ReviewForm;
--- Client/src/pages/Review.jsx
+++ Client/src/pages/Review.jsx
@@ -1,29 +1,39 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Client from '../services/api';
+import ReviewForm from '../components/ReviewForm';
 
 const Review = () => {
   const [books, setBooks] = useState([]);
+  const [selectedBookId, setSelectedBookId] = useState(null);
 
   const fetchBooks = async () => {
-    const response = await Client.get('/books');
-    setBooks(response.data);
-  }
+    try {
+      const response = await Client.get('/books');
+      setBooks(response.data);
+    } catch (error) {
+      console.error('Error fetching books:', error);
+    }
+  };
+
+  const handleBookClick = (bookId) => {
+    setSelectedBookId(bookId);
+  };
 
   useEffect(() => {
     fetchBooks();
-  }, [])
+  }, []);
 
   return (
     <div className="container">
       <h2>Your Library</h2>
-      {books.map(book => (
+      {books.map((book) => (
         <div key={book.id}>
-          <h3>{book.title}</h3>
-          
+          <h3 onClick={() => handleBookClick(book.id)}>{book.title}</h3>
+          {selectedBookId === book.id && <ReviewForm bookId={book.id} />}
         </div>
       ))}
     </div>
   );
-}
+};
 
 export default Review;
