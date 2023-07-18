e3d74afac296495c2080e9edfc0b0136757ad922
Jorge Islas <Jorgeislas97@gmail.com>
2023-07-18 12:55:02 -0400

    Review page is rendering books

--- Client/src/pages/Review.jsx
+++ Client/src/pages/Review.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
 import { useState, useEffect } from 'react';
 import Client from '../services/api';
 
 const Review = () => {
+  const [books, setBooks] = useState([]);
+
+  const fetchBooks = async () => {
+    const response = await Client.get('/books');
+    setBooks(response.data);
+  }
+
+  useEffect(() => {
+    fetchBooks();
+  }, [])
+
   return (
-    <div>
-      <h1>helloooooooooo</h1>
+    <div className="container">
+      <h2>Your Library</h2>
+      {books.map(book => (
+        <div key={book.id}>
+          <h3>{book.title}</h3>
+          
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Review
+export default Review;
